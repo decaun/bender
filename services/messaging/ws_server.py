53beb54d4a3eaea086df26e06c571030de396cd9
decaun <denizcanuner@gmail.com>
2019-08-24 01:32:06 +0200

    old service removed

--- services/messaging/ws_server.py
+++ services/messaging/ws_server.py
@@ -5,68 +5,64 @@ import websockets
 
 logging.basicConfig()
 
-STATE={
-    "type":"USERS_LIST",
-    "users":[]
+STATE = {
+    "type": "USERS_LIST",
+    "users": []
 }
 
+USERS = dict()
 
-USERS = set()
-USERZ=dict()
 
 def join_event():
-    STATE['users']=list(USERZ.values())
-    print(STATE)
+    STATE['users'] = list(USERS.values())
     return json.dumps(STATE)
 
 
 def message_event(data):
-    
-    return json.dumps({"type": "ADD_MESSAGE", "message": data['message'],"author":data['author']})
+
+    return json.dumps({"type": "ADD_MESSAGE", "message": data['message'], "author": data['author']})
 
 
 async def update_users():
     if USERS:  # asyncio.wait doesn't accept an empty list
-        
+
         message = join_event()
-        await asyncio.wait([user.send(message) for user in USERS])
+        await asyncio.wait([user.send(message) for user in USERS.keys()])
 
 
-async def update_messages(sender,data):
+async def update_messages(sender, data):
     if USERS:  # asyncio.wait doesn't accept an empty list
         message = message_event(data)
-        await asyncio.wait([user.send(message) for user in USERS if user!=sender])
+        await asyncio.wait([user.send(message) for user in USERS.keys() if user != sender])
 
 
 async def register(websocket):
-    USERS.add(websocket)
-    USERZ.update({websocket:[]})
-    #await update_messages()
+    USERS.update({websocket: []})
+    # await update_messages()
 
 
 async def unregister(websocket):
-    USERS.remove(websocket)
-    del USERZ[websocket]
+    del USERS[websocket]
     STATE['users'].pop()
-    print('deleted')
-    #await update_messages()
+    # await update_messages()
     await update_users()
 
 
 async def counter(websocket, path):
     # register(websocket) sends user_event() to websocket
-    await register(websocket)
+    
     try:
-        #await websocket.send(join_event())
+        # await websocket.send(join_event())
         async for message in websocket:
             data = json.loads(message)
             if data["type"] == "ADD_USER":
-                USERZ[websocket]=({"name":data['name'],"id":len(USERZ)})
-                #STATE['users'].append({"name":data['name'],"id":len(USERS)+1})
-                #print(json.dumps(list(USERZ.values())))
+                await register(websocket)
+                USERS[websocket] = ({"name": data['name'], "id": len(USERS)})
+                # STATE['users'].append({"name":data['name'],"id":len(USERS)+1})
+                # print(json.dumps(list(USERS.values())))
                 await update_users()
             elif data["type"] == "ADD_MESSAGE":
-                await update_messages(websocket,data)
+                await update_messages(websocket, data)
                 pass
             else:
                 logging.error("unsupported event: {}", data)
